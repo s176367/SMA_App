2fe23544b685d311ea1ab6d61d91804a0cea4ea8
Sbicen99 <sercanbicen99@gmail.com>
2019-10-28 17:52:41 +0100

    Nyt branch

---
+++ app/src/main/java/com/example/sma/Aktivitet1.java
@@ -0,0 +1,21 @@
+package com.example.sma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+public class Aktivitet1 extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aktivitet1);
+
+
+    }
+}
+
+
