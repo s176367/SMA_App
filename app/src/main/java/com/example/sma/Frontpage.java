d48fc749f11024e4947f584613419cd8755d1454
Gustav <gust1224@live.dk>
2019-10-29 10:30:46 +0100

    Starter forfra med fragment

--- app/src/main/java/com/example/sma/Frontpage.java
+++ app/src/main/java/com/example/sma/Frontpage.java
@@ -8,35 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Frontpage extends AppCompatActivity implements View.OnClickListener {
+public class Frontpage{
 
 
-    Button account;
-    Button favorites;
-    Button meetings;
-    Button invitations;
-    Button create;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.frontpage);
 
-        account = findViewById(R.id.account);
-        meetings = findViewById(R.id.meetings);
-        favorites = findViewById(R.id.favorites);
-        invitations = findViewById(R.id.invitations);
-        create = findViewById(R.id.create);
 
-        meetings.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(getApplicationContext(), FragmentContainer.class);
-        startActivity(intent);
-    }
 }
 
