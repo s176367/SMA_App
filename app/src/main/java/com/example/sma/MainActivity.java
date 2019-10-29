c52063ce8eae6ee365a661fd9d35ee0c9f1c7d0c
Gustav <gust1224@live.dk>
2019-10-29 11:11:40 +0100

    Nu virker de tre fragmenter :)

---
+++ app/src/main/java/com/example/sma/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.sma;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragmemt = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragmemt = new HomeFragment();
+                            break;
+
+                        case R.id.nav_meetings:
+                            selectedFragmemt = new MeetingFragment();
+                            break;
+
+                        case R.id.nav_invites:
+                            selectedFragmemt = new invitesFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragmemt).commit();
+                    return true;
+                }
+            };
+}
