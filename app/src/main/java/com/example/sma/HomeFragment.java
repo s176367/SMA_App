455f9640a7e1d2e27e945f6f56e2208e14492998
Gustav <gust1224@live.dk>
2019-10-29 20:42:45 +0100

    Scroll ting er federe nu

--- app/src/main/java/com/example/sma/HomeFragment.java
+++ app/src/main/java/com/example/sma/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.sma;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,10 +16,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     RecyclerView recyclerView;
     MeetingAdapter adapter;
+    Button but_create;
 
     List<MeetingCard> meetingList;
 
@@ -31,41 +34,36 @@ public class HomeFragment extends Fragment {
         meetingList = new ArrayList<>();
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView2);
         recyclerView.setHasFixedSize(true);
+        recyclerView.setNestedScrollingEnabled(false);
 
+
+        but_create = view.findViewById(R.id.but_createMeeting);
+        but_create.setOnClickListener(this);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
 
-        meetingList.add(
-                new MeetingCard(0,"Møde", "19:00-20:00", "Ballerup DTU R2", 10)
+       // fake meetings for prototype
+        meetingList.add(new MeetingCard(0,"Møde", "19:00-20:00", "Ballerup DTU R2", 10)
         );
-
-        meetingList.add(
-                new MeetingCard(1,"Vigtigt møde", "19:00-21:00", "Ballerup DTU R4", 4)
+        meetingList.add(new MeetingCard(1,"Vigtigt møde", "19:00-21:00", "Ballerup DTU R4", 4)
+        );
+        meetingList.add(new MeetingCard(2,"Vigtigt møde", "19:00-21:00", "Ballerup DTU R4", 4)
         );
         meetingList.add(
-                new MeetingCard(2,"Vigtigt møde", "19:00-21:00", "Ballerup DTU R4", 4)
-        );
-
+                new MeetingCard(1,"Vigtigt møde", "19:00-21:00", "Ballerup DTU R4", 4));
         meetingList.add(
-                new MeetingCard(1,"Vigtigt møde", "19:00-21:00", "Ballerup DTU R4", 4)
-        );
-
-        meetingList.add(
-                new MeetingCard(1,"Vigtigt møde", "19:00-21:00", "Ballerup DTU R4", 4)
-        );
-
+                new MeetingCard(1,"Vigtigt møde", "19:00-21:00", "Ballerup DTU R4", 4));
         adapter = new MeetingAdapter(getContext(), meetingList);
         recyclerView.setAdapter(adapter);
-
         return view;
+    }
 
+    @Override
+    public void onClick(View view) {
 
+        if (view == but_create){
 
-
-
-
-
-
+        }
     }
 }
