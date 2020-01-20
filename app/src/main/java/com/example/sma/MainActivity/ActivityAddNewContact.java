468c972c497fcabf814f13a9f5dab97f6d838645
Sbicen99 <sercanbicen99@gmail.com>
2020-01-20 17:32:17 +0100

    Små layout-forbedringer

--- app/src/main/java/com/example/sma/MainActivity/ActivityAddNewContact.java
+++ app/src/main/java/com/example/sma/MainActivity/ActivityAddNewContact.java
@@ -1,17 +1,23 @@
 package com.example.sma.MainActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sma.Database.FirebaseControl;
 import com.example.sma.Database.SenderCallback;
 import com.example.sma.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ActivityAddNewContact extends AppCompatActivity {
 
@@ -46,9 +52,34 @@ public class ActivityAddNewContact extends AppCompatActivity {
                 }
 
 
-            }
+                final String content = inputEmailCheck.getText().toString();
+                FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+                rootRef.collection("users").whereEqualTo("email", content).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document : task.getResult()) {
+                                if (document.getString("email").equals(content)) {
 
-            });
+                                    Toast.makeText(ActivityAddNewContact.this, "Invitation send", Toast.LENGTH_SHORT).show();
+                                    Log.d(TAG, "Successfull", task.getException());
+
+                                } else {
+
+                                    Toast.makeText(ActivityAddNewContact.this, "Invitation isn't send", Toast.LENGTH_SHORT).show();
+                                    Log.d(TAG, "Not successfull");
+
+                                }
+                            }
+
+                        }
+                    }
+
+                });
+
+
+                }
+         });
     }
 
 
